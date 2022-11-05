da4b9134c636ac60d64b2314ea3f8cb83970afa4
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-05 14:33:17 -0700

    Added petr sprite

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import { particle } from "./petr.js"
 
 const SCREEN_HEIGHT = window.innerHeight;
 const SCREEN_WIDTH = window.innerWidth;
-let petrParticle = new particle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0, 0);
+let petrParticle = new particle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0);
 petrParticle.randomize_angle();
-petrParticle.set_speed(5);
+petrParticle.set_speed(0.25);
 
 function positionElement(el, x, y) {
     el.style.position = 'absolute';
@@ -12,34 +12,45 @@ function positionElement(el, x, y) {
     el.style.top = y + 'px';
 }
 
-let petr = document.querySelector('.petr');
+const petr = document.querySelector('.petr');
 
 function updatePetr() {
-    petrParticle.move()
+    // console.log(petrParticle.get_angle(), petrParticle.get_speed())
+    petrParticle.move();
+    let chance = Math.floor(Math.random() * 100)
+    if (chance === 5) {petrParticle.randomize_angle()}
     let [x, y] = petrParticle.get_position();
     positionElement(petr, x, y);
 }
 
-setInterval(updatePetr, 100)
+setInterval(updatePetr, 5)
 
-// const example = {
-//     "ip": "0.0.0.0",
-//     "username": "peteranteater",
-//     "mood": "happy",
-//     "petr_sprite": "normal_petr",
-//     "hunger_value": 100,
-//     "clean_level": 100,
-//     "boredom_level": 100
-// }
+const imagePaths = {
+    "normal_petr": "./images/petr.png"
+}
 
-// function loadPetr(petr) {
-//     document.querySelector('.username').textContent = `username: ${petr.username}`
-//     document.querySelector('.hunger').textContent = `hunger: ${petr.hunger_value}`
-//     document.querySelector('.clean').textContent = `clean: ${petr.clean_level}`
-//     document.querySelector('.boredom').textContent = `boredom: ${petr.boredom_level}`
+const example = {
+    "ip": "0.0.0.0",
+    "username": "peteranteater",
+    "mood": "happy",
+    "petr_sprite": "normal_petr",
+    "hunger_value": 100,
+    "clean_level": 100,
+    "boredom_level": 100
+}
 
-//     // load sprite
-//     // load mood
-// }
+function loadPetr(petr) {
+    document.querySelector('.username').textContent = `username: ${petr.username}`
+    document.querySelector('.hunger').textContent = `hunger: ${petr.hunger_value}`
+    document.querySelector('.clean').textContent = `clean: ${petr.clean_level}`
+    document.querySelector('.boredom').textContent = `boredom: ${petr.boredom_level}`
 
-// loadPetr(example)
+    const img = document.createElement("img")
+    img.src = imagePaths[petr.petr_sprite]
+    const insert = document.querySelector('.petr');
+    insert.appendChild(img)
+    // load sprite
+    // load mood
+}
+
+loadPetr(example)
