9f15de472c3298014ad5dfe5ce97715b086555a1
aaronist <aaronmblume@gmail.com>
2022-11-05 12:26:05 -0700

    Petrgotchi update mood when increasing attributes

--- Petrgotchi.py
+++ Petrgotchi.py
@@ -3,28 +3,30 @@ MAX = 10000
 HUNGER_INCREASE_RATE = 50
 HUNGER_DECREASE_RATE = 5
 
-BOREDOM_INCREASE_RATE = 100
-BOREDOM_DECREASE_RATE = 1
+ENTERTAINMENT_INCREASE_RATE = 100
+ENTERTAINMENT_DECREASE_RATE = 1
 
 CLEANLINESS_INCREASE_RATE = 5000
 CLEANLINESS_DECREASE_RATE = 2
 
 
 class Petrgotchi:
-    def __init__(self):
+    def __init__(self, **kargs):
         self._hunger = MAX
-        self._boredom = MAX
+        self._entertainment = MAX
         self._cleanliness = MAX
 
         self._mood = "neutral"
+
+        self._sprite = kargs["sprite"]
     
 
     def hunger(self) -> int:
         return self._hunger
     
 
-    def boredom(self) -> int:
-        return self._boredom
+    def entertainment(self) -> int:
+        return self._entertainment
     
 
     def cleanliness(self) -> int:
@@ -36,10 +38,20 @@ class Petrgotchi:
     
 
     def increase_hunger(self) -> None:
+        # Increase the hunger
         if self._hunger > MAX - HUNGER_INCREASE_RATE:
             self._hunger = MAX
         else:
             self._hunger += HUNGER_INCREASE_RATE
+        
+        # Update the mood if necessary
+        if self._mood != "neutral":
+            if self._entertainment == 0:
+                self._mood = "sad"
+            elif self._cleanliness == 0:
+                self._mood = "stinky"
+        else:
+            self._mood = "neutral"
 
     
     def decrease_hunger(self) -> None:
@@ -54,30 +66,50 @@ class Petrgotchi:
             self._mood = "dead"
     
 
-    def increase_boredom(self) -> None:
-        if self._boredom > MAX - BOREDOM_INCREASE_RATE:
-            self._boredom = MAX
+    def increase_entertainment(self) -> None:
+        # Increase the entertainment
+        if self._entertainment > MAX - ENTERTAINMENT_INCREASE_RATE:
+            self._entertainment = MAX
         else:
-            self._boredom += BOREDOM_INCREASE_RATE
-    
-
-    def decrease_boredom(self) -> None:
-        # Decrease the boredom
-        if self._boredom - BOREDOM_DECREASE_RATE <= 0:
-            self._boredom = 0
-        else:
-            self._boredom -= BOREDOM_DECREASE_RATE
+            self._entertainment += ENTERTAINMENT_INCREASE_RATE
         
         # Update the mood if necessary
-        if self._boredom == 0 and self._mood == "neutral":
+        if self._mood != "neutral":
+            if self._hunger == 0:
+                self._mood = "dead"
+            elif self._cleanliness == 0:
+                self._mood = "stinky"
+        else:
+            self._mood = "neutral"
+    
+
+    def decrease_entertainment(self) -> None:
+        # Decrease the entertainment
+        if self._entertainment - ENTERTAINMENT_DECREASE_RATE <= 0:
+            self._entertainment = 0
+        else:
+            self._entertainment -= ENTERTAINMENT_DECREASE_RATE
+        
+        # Update the mood if necessary
+        if self._entertainment == 0 and self._mood == "neutral":
             self._mood = "sad"
     
 
     def increase_cleanliness(self) -> None:
+        # Increase the cleanliness
         if self._cleanliness > MAX - CLEANLINESS_INCREASE_RATE:
             self._cleanliness = MAX
         else:
             self._cleanliness += CLEANLINESS_INCREASE_RATE
+        
+        # Update the mood if necessary
+        if self._mood != "neutral":
+            if self._hunger == 0:
+                self._mood = "dead"
+            elif self._entertainment == 0:
+                self._mood = "sad"
+        else:
+            self._mood = "neutral"
 
     
     def decrease_cleanliness(self) -> None:
