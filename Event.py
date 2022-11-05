0acc0b1401b8cf729baddc3746ca37200b6b7658
Nathan Monette <nathanmonette1@gmail.com>
2022-11-05 12:11:47 -0700

    Deleted Duplicate File

---
+++ Event.py
@@ -0,0 +1,38 @@
+import asyncio
+from collections import defaultdict
+
+class Tasks:
+    def __init__(self):
+        self.hunger = None
+        self.clean = None
+        self.bored = None
+
+class Event:
+    def __init__(self):
+        self.event_loop = asyncio.new_event_loop
+        self.petrs = dict()
+        self.tasks = defaultdict(Tasks) #[hunger, play, clean]
+
+    async def register("params"):
+        self.petrs[]Petrgatchi(**kargs)
+        # use asyncio.gather(feed(), shower(), play())
+
+    async def feed(self, "params"):
+        self.tasks[].hunger = asyncio.create_task(create_hunger())
+        self.tasks[][0].cancel()
+        self.tasks[][0] = self.create_task(self.create_hunger())
+        try:
+            await asyncio.sleep("value")
+        except asyncio.CancelledError:
+            
+    async def create_hunger(self, "params"):
+        try:
+            await asyncio.sleep("time")
+        except asyncio.CancelledError:
+            self.petrs["username"].
+            if self.petrs["username"] == 0:
+                pass # do some interaction with the UI that demonstrates it has died
+ 
+        
+
+
