b11934ac2377b55c566188d37ab41b5c6260950e
Nathan Monette <nathanmonette1@gmail.com>
2022-11-05 18:03:17 -0700

    attempting to test js

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 import { particle } from "./petr.js"
 
-// function checkIfUserExists() {
-//     http = new XMLHttpRequest;
-//     const url = "FILL THIS IN WITH THE REAL URL"
-//     http.open("GET", url)
-//     http.send()
-//     console.log(http.responseText)
-// }
+function checkIfUserExists() {
+    let http = new XMLHttpRequest;
+    const url = "http://127.0.0.1:8000/register?username=nmonette"
+    http.open("GET", url)
+    http.send()
+    fetch("http://127.0.0.1:8000/register", {
+        method: "POST", 
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+            ip: "169.234.33.95",
+            sprite: "sprite"
+        })
+    })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))
+}
 
-// checkIfUserExists()
+checkIfUserExists()
 
 const SCREEN_HEIGHT = window.innerHeight;
 const SCREEN_WIDTH = window.innerWidth;
